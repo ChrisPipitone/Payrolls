00edd22797c765e83ab45d411278851b30717d8e
avalosFivan <83251028+avalosFivan@users.noreply.github.com>
2021-10-10 13:11:21 -0400

    Admin

--- Payrolls/Form1.h
+++ Payrolls/Form1.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "MyForm.h"
 
 namespace Payrolls {
 
@@ -78,7 +79,7 @@ namespace Payrolls {
 			this->label1->Location = System::Drawing::Point(462, 53);
 			this->label1->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(526, 44);
+			this->label1->Size = System::Drawing::Size(413, 36);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Welcome to Hmoney Payroll";
 			// 
@@ -87,7 +88,7 @@ namespace Payrolls {
 			this->label2->AutoSize = true;
 			this->label2->Location = System::Drawing::Point(440, 270);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(162, 29);
+			this->label2->Size = System::Drawing::Size(129, 24);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Employee ID";
 			this->label2->Click += gcnew System::EventHandler(this, &Form1::label2_Click);
@@ -97,7 +98,7 @@ namespace Payrolls {
 			this->label3->AutoSize = true;
 			this->label3->Location = System::Drawing::Point(465, 323);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(128, 29);
+			this->label3->Size = System::Drawing::Size(100, 24);
 			this->label3->TabIndex = 2;
 			this->label3->Text = L"Password";
 			this->label3->Click += gcnew System::EventHandler(this, &Form1::label3_Click);
@@ -107,7 +108,7 @@ namespace Payrolls {
 			this->label4->AutoSize = true;
 			this->label4->Location = System::Drawing::Point(494, 385);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(108, 29);
+			this->label4->Size = System::Drawing::Size(84, 24);
 			this->label4->TabIndex = 3;
 			this->label4->Text = L"Position";
 			// 
@@ -115,14 +116,14 @@ namespace Payrolls {
 			// 
 			this->textBox1->Location = System::Drawing::Point(646, 270);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(275, 34);
+			this->textBox1->Size = System::Drawing::Size(275, 28);
 			this->textBox1->TabIndex = 4;
 			// 
 			// textBox2
 			// 
 			this->textBox2->Location = System::Drawing::Point(646, 323);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(275, 34);
+			this->textBox2->Size = System::Drawing::Size(275, 28);
 			this->textBox2->TabIndex = 5;
 			this->textBox2->TextChanged += gcnew System::EventHandler(this, &Form1::textBox2_TextChanged);
 			// 
@@ -132,7 +133,7 @@ namespace Payrolls {
 			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"HR", L"Admin", L"Employee" });
 			this->comboBox1->Location = System::Drawing::Point(646, 385);
 			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(281, 37);
+			this->comboBox1->Size = System::Drawing::Size(281, 30);
 			this->comboBox1->TabIndex = 6;
 			// 
 			// button1
@@ -143,10 +144,11 @@ namespace Payrolls {
 			this->button1->TabIndex = 7;
 			this->button1->Text = L"Login";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
 			// Form1
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(15, 29);
+			this->AutoScaleDimensions = System::Drawing::SizeF(11, 22);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::BlanchedAlmond;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
@@ -178,6 +180,8 @@ private: System::Void textBox2_TextChanged(System::Object^ sender, System::Event
 	this->textBox2->PasswordChar = '*';
 }
 private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
