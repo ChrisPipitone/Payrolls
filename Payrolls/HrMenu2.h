04980c5b11f0173e0f2161dff141d62413c11284
UltimaPat <patrick.chu.131@gmail.com>
2021-10-27 11:17:21 -0400

    adding removeEmployee window

--- Payrolls/HrMenu2.h
+++ Payrolls/HrMenu2.h
@@ -9,36 +9,29 @@ namespace Payrolls {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	/// <summary>
-	/// Zusammenfassung für Form1
-	/// </summary>
-	public ref class MyForm : public System::Windows::Forms::Form
+	public ref class HRMenu2 : public System::Windows::Forms::Form
 	{
 	public:
-		MyForm(void)
+		HRMenu2(void)
 		{
 			InitializeComponent();
-			//textBox1->EmplText = "A";
-			//
-			//TODO: Konstruktorcode hier hinzufügen.
-			//
+
 		}
 
 	protected:
-		/// <summary>
-		/// Verwendete Ressourcen bereinigen.
-		/// </summary>
-		~MyForm()
+
+		~HRMenu2()
 		{
 			if (components)
 			{
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^ button1; //Search Employee
+	private: System::Windows::Forms::Button^ button1; //Search For Current Employee
 	private: System::Windows::Forms::Button^ button2; //Add Employee	
 	private: System::Windows::Forms::Button^ button3; //Remove Employee
 	private: System::Windows::Forms::Button^ button4; //Logout
+	private: System::Windows::Forms::Button^ button5; //View Tax Codes
 
 	private: System::Windows::Forms::TextBox^ Adm_txt;
 
@@ -57,22 +50,18 @@ namespace Payrolls {
 	protected:
 
 	private:
-		/// <summary>
-		/// Erforderliche Designervariable.
-		/// </summary>
+
 		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Erforderliche Methode für die Designerunterstützung.
-		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
-		/// </summary>
+
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
@@ -84,11 +73,11 @@ namespace Payrolls {
 				static_cast<System::Byte>(0)));
 			this->button1->Location = System::Drawing::Point(212, 150);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(185, 30);
+			this->button1->Size = System::Drawing::Size(213, 30);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"Search Employee";
+			this->button1->Text = L"Search For Current Employee";
 			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			this->button1->Click += gcnew System::EventHandler(this, &HRMenu2::button1_Click);
 			// 
 			// button2
 			// 
@@ -96,11 +85,11 @@ namespace Payrolls {
 				static_cast<System::Byte>(0)));
 			this->button2->Location = System::Drawing::Point(212, 186);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(185, 32);
+			this->button2->Size = System::Drawing::Size(213, 32);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"Add Employee";
 			this->button2->UseVisualStyleBackColor = true;
-			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			this->button2->Click += gcnew System::EventHandler(this, &HRMenu2::button2_Click);
 			// 
 			// button3
 			// 
@@ -108,11 +97,11 @@ namespace Payrolls {
 				static_cast<System::Byte>(0)));
 			this->button3->Location = System::Drawing::Point(212, 224);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(185, 32);
+			this->button3->Size = System::Drawing::Size(213, 32);
 			this->button3->TabIndex = 1;
 			this->button3->Text = L"Remove Employee";
 			this->button3->UseVisualStyleBackColor = true;
-			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			this->button3->Click += gcnew System::EventHandler(this, &HRMenu2::button3_Click);
 			// 
 			// button4
 			// 
@@ -120,11 +109,23 @@ namespace Payrolls {
 				static_cast<System::Byte>(0)));
 			this->button4->Location = System::Drawing::Point(212, 262);
 			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(185, 32);
+			this->button4->Size = System::Drawing::Size(213, 33);
 			this->button4->TabIndex = 1;
 			this->button4->Text = L"Log out";
 			this->button4->UseVisualStyleBackColor = true;
-			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			this->button4->Click += gcnew System::EventHandler(this, &HRMenu2::button4_Click);
+			// 
+			// button5
+			// 
+			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button5->Location = System::Drawing::Point(212, 306);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(213, 32);
+			this->button5->TabIndex = 1;
+			this->button5->Text = L"View Tax Codes";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &HRMenu2::button5_Click);
 			// 
 			// label1
 			// 
@@ -136,7 +137,7 @@ namespace Payrolls {
 			this->label1->Size = System::Drawing::Size(239, 31);
 			this->label1->TabIndex = 3;
 			this->label1->Text = L"Human Resources";
-			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			this->label1->Click += gcnew System::EventHandler(this, &HRMenu2::label1_Click);
 			// 
 			// label2
 			// 
@@ -151,13 +152,14 @@ namespace Payrolls {
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(100, 23);
 			this->label3->TabIndex = 8;
-			this->label3->Click += gcnew System::EventHandler(this, &MyForm::label3_Click_1);
+			this->label3->Click += gcnew System::EventHandler(this, &HRMenu2::label3_Click_1);
 			// 
-			// MyForm
+			// HRMenu2
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(622, 435);
+			this->BackColor = System::Drawing::Color::BlanchedAlmond;
+			this->ClientSize = System::Drawing::Size(700, 500);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
@@ -165,8 +167,9 @@ namespace Payrolls {
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button4);
-			this->Name = L"MyForm";
+			this->Name = L"HRMenu2";
 			this->Text = L"HRMenu2";
+			this->Load += gcnew System::EventHandler(this, &HRMenu2::MyForm_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -176,17 +179,14 @@ namespace Payrolls {
 
 		String^ L = L"kjkj";
 
-		//ADMform f2 = new Form2();
-
-
-
-
 	}
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
@@ -202,5 +202,7 @@ namespace Payrolls {
 	}
 	private: System::Void label3_Click_1(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
