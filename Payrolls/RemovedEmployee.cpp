a3ee7f3ca692de9c661c49f09084ddd5171584a1
UltimaPat <patrick.chu.131@gmail.com>
2021-11-03 02:01:13 -0400

    remove employee function

---
+++ Payrolls/RemovedEmployee.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "removeEmployee.h"
+#include "Employee.h"
+
+using namespace std;
+
+/*void removeEmployee::deleteEmployee() {
+	delete Employee.firstName; 
+	delete Employee.lastName;
+	delete Employee.SSN;
+	delete Employee.dateOfBirth;
+	delete Employee.homeAddress;
+}*/
