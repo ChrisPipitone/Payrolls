8fd1d59836b48de69703aca5a84077a53ff7e6c6
samhu911 <zhuowei.hu@cix.csi.cuny.edu>
2021-12-12 21:30:00 -0500

    finished

--- Payrolls/ConnectionPath.h
+++ Payrolls/ConnectionPath.h
@@ -2,6 +2,9 @@
 using namespace System;
 
 ref struct ConnectionPath {
+
 	static System::String^ connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:/Users/Zhuowei Hu/Downloads/Payroll_InfoDone.accdb";
 	static System::String^ connectionPrevious = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:/Users/Zhuowei Hu/Downloads/PreviousEmployees.accdb";
+
+
 };
