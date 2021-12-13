d6d42b71b22440102e8498755195c24a47da1134
avalosFivan <83251028+avalosFivan@users.noreply.github.com>
2021-12-12 23:03:46 -0500

    ok

--- Payrolls/ConnectionPath.h
+++ Payrolls/ConnectionPath.h
@@ -3,7 +3,7 @@ using namespace System;
 
 ref struct ConnectionPath {
 
-	static System::String^ connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:/Users/Zhuowei Hu/Downloads/Payroll_InfoDone.accdb";
+	static System::String^ connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:/Users/Ivan/Desktop/Payroll_Info1.accdb";
 	static System::String^ connectionPrevious = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:/Users/Zhuowei Hu/Downloads/PreviousEmployees.accdb";
 
 
