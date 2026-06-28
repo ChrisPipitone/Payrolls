536486bae909ca9cc2ac5e8e0443d7b86e2333ad
Chris Pipitone <chrispipitone@protonmail.com>
2026-06-27 21:28:23 -0400

    starting menu init

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,138 @@
-#include <ncurses.h>
+#include <cstdlib>
+#include <curses.h>
+#include <menu.h>
+#include <string.h>
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define CTRLD 4
+
+struct RoleMenuChoice {
+  const char *choice_name;
+  const char *description;
+};
+
+constexpr RoleMenuChoice role_menu_choice[] = {
+    {"Employee", "- Basic Employee View"},
+    {"HR", "- HR View"},
+    {"Manager", "- Manager View"},
+    {"Exit", ""},
+};
+
+void print_in_middle(WINDOW *win, int starty, int startx, int width,
+                     char *string, chtype color);
+
+// These will be moved l8r
+void employee_view() {}
+void hr_view() {}
+void manager_view() {}
 
 int main() {
-  initscr();                 /* Start curses mode 		  */
-  printw("Hello World !!!"); /* Print Hello World		  */
-  refresh();                 /* Print it on to the real screen */
-  getch();                   /* Wait for user input */
-  endwin();                  /* End curses mode		  */
+  ITEM **my_items;
+  int c;
+  MENU *my_menu;
+  WINDOW *my_menu_win;
+  int n_choices, i;
+  bool exit_selected = false;
 
-  return 0;
+  /* Initialize curses */
+  initscr();
+  start_color();
+  cbreak();
+  noecho();
+  keypad(stdscr, TRUE);
+  init_pair(1, COLOR_RED, COLOR_BLACK);
+
+  /* Create items */
+  n_choices = ARRAY_SIZE(role_menu_choice);
+  my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
+  for (i = 0; i < n_choices; ++i)
+    my_items[i] = new_item(role_menu_choice[i].choice_name,
+                           role_menu_choice[i].description);
+
+  /* Crate menu */
+  my_menu = new_menu((ITEM **)my_items);
+
+  /* Create the window to be associated with the menu */
+  my_menu_win = newwin(10, 40, 4, 4);
+  keypad(my_menu_win, TRUE);
+
+  /* Set main window and sub window */
+  set_menu_win(my_menu, my_menu_win);
+  set_menu_sub(my_menu, derwin(my_menu_win, 6, 38, 3, 1));
+
+  /* Set menu mark to the string " * " */
+  set_menu_mark(my_menu, " * ");
+
+  /* Print a border around the main window and print a title */
+  box(my_menu_win, 0, 0);
+  print_in_middle(my_menu_win, 1, 0, 40, "Main Menu", COLOR_PAIR(1));
+  mvwaddch(my_menu_win, 2, 0, ACS_LTEE);
+  mvwhline(my_menu_win, 2, 1, ACS_HLINE, 38);
+  mvwaddch(my_menu_win, 2, 39, ACS_RTEE);
+  mvprintw(LINES - 2, 0, "F1 to exit");
+  refresh();
+
+  /* Post the menu */
+  post_menu(my_menu);
+  wrefresh(my_menu_win);
+
+  while (!exit_selected && (c = wgetch(my_menu_win)) != KEY_F(1)) {
+    switch (c) {
+    case KEY_DOWN:
+      menu_driver(my_menu, REQ_DOWN_ITEM);
+      break;
+    case KEY_UP:
+      menu_driver(my_menu, REQ_UP_ITEM);
+      break;
+    case '\n':
+    case KEY_ENTER:
+      int idx = item_index(current_item(my_menu));
+      switch (idx) {
+      case 0:
+        employee_view();
+        break;
+      case 1:
+        hr_view();
+        break;
+      case 2:
+        manager_view();
+        break;
+      case 3:
+        exit_selected = true;
+      }
+      break;
+    }
+    wrefresh(my_menu_win);
+  }
+
+  /* Unpost and free all the memory taken up */
+  unpost_menu(my_menu);
+  free_menu(my_menu);
+  for (i = 0; i < n_choices; ++i)
+    free_item(my_items[i]);
+  endwin();
+}
+
+void print_in_middle(WINDOW *win, int starty, int startx, int width,
+                     char *string, chtype color) {
+  int length, x, y;
+  int temp;
+
+  if (win == NULL)
+    win = stdscr;
+  getyx(win, y, x);
+  if (startx != 0)
+    x = startx;
+  if (starty != 0)
+    y = starty;
+  if (width == 0)
+    width = 80;
+
+  length = strlen(string);
+  temp = (width - length) / 2;
+  x = startx + (int)temp;
+  wattron(win, color);
+  mvwprintw(win, y, x, "%s", string);
+  wattroff(win, color);
+  refresh();
 }
