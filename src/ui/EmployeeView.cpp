614cb81c9b1949bfab3f88e3ff0aed46e1800ddb
Chris Pipitone <chrispipitone@protonmail.com>
2026-07-23 16:07:37 -0400

    feat: full header and help hint shows correctly

--- src/ui/EmployeeView.cpp
+++ src/ui/EmployeeView.cpp
@@ -1,19 +1,21 @@
 #include "payrolls/ui/EmployeeView.h"
+
 #include "payrolls/ui/utils.h"
 
 void EmployeeView::draw() {
   box(view_win, 0, 0);
-  print_in_middle(view_win, 1, 0, 40, "Employee View", COLOR_PAIR(1));
+  int w = getmaxx(view_win);
+  print_in_middle(view_win, 1, 0, w, "Employee View", COLOR_PAIR(1));
   mvwaddch(view_win, 2, 0, ACS_LTEE);
-  mvwhline(view_win, 2, 1, ACS_HLINE, 38);
-  mvwaddch(view_win, 2, 39, ACS_RTEE);
-  refresh();
+  mvwhline(view_win, 2, 1, ACS_HLINE, w - 2);
+  mvwaddch(view_win, 2, w - 1, ACS_RTEE);
   wrefresh(view_win);
 }
 
 void EmployeeView::render() {
-  mvprintw(LINES - 3, 0, "q to exit EmployeeView");
   bool exit_selected = false;
+  wclear(view_win);
+  mvwprintw(view_win, LINES - 3, 3, "q to exit EmployeeView");
   int c = 0;
   do {
     draw();
@@ -21,9 +23,4 @@ void EmployeeView::render() {
 
   werase(view_win);
   wrefresh(view_win);
-
-  // exit: clear stdscr line
-  move(LINES - 3, 0);
-  clrtoeol();
-  refresh();
 }
