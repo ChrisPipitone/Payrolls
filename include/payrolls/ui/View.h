e7d2a75e5e22beeafc8a325db45f7d313307f2ab
Chris Pipitone <chrispipitone@protonmail.com>
2026-09-05 13:15:23 -0400

    feat: add proper return to get_first_leaf and move it to layout then use it

--- include/payrolls/ui/View.h
+++ include/payrolls/ui/View.h
@@ -34,7 +34,6 @@ class View {
   LayoutNode root_node;
   virtual const std::vector<KeyHint>& hints() const = 0;
   Section* focused_ = nullptr;
-  virtual Section* get_first_leaf(LayoutNode& node);
   bool change_focused_section(Dir direction);
   Rect content_rect() const;
 
